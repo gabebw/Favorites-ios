176c2ff3f4fc48fef0e48ac952800cf0ca2700e1
Gabe Berke-Williams <gabebw@gabebw.com>
2014-09-10 11:48:47 -0400

    Display dummy data in table view

--- Favorites/FAVMasterViewController.m
+++ Favorites/FAVMasterViewController.m
@@ -25,6 +25,41 @@
     // Create array of editable items
     // Note that we're not doing alloc-init
     self.favorites = [NSMutableArray array];
+
+    // Create inline mutable array
+    self.favorites = [@[@"HELLO", @"HI", @"THERE"] mutableCopy];
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+// method name is tableView, takes 2 parameters:
+// (UITableView *)tableView
+// (NSInteger)section
+// to call it:
+//
+// rowCount = [thingy tableView:self numberOfRowsInSection:6]
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return (NSInteger)[self.favorites count];
+}
+
+// This method is called when iOS needs to populate a specific table cell
+// with content
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSInteger row = indexPath.row;
+    // Grab a specific favorite (`id` is a type that can be a pointer to anything)
+
+    // Old:
+    //id favorite = [self.favorites objectAtIndex:row];
+    // New hotness:
+    id favorite = self.favorites[row];
+
+    // Makes cells the first 10 or 12 times, then reuses them every time after that
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"StandardCell" forIndexPath:indexPath];
+    // Set cell contents
+    cell.textLabel.text = [favorite description];
+
+    return cell;
+}
 @end
